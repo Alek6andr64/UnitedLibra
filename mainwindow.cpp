ccc31af855dd64b4374dbf6165837002ea921770
MasterEl1te <pushkarskij1999@bk.ru>
2026-04-29 00:58:48 +0700

    Feat: Tabs and Redaction Window prototype

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,14 +15,34 @@ MainWindow::MainWindow(QWidget *parent)
     // Центральный виджет
     QWidget *central = new QWidget(this);
     QHBoxLayout *rootLayout = new QHBoxLayout(central);
+    rootLayout->setContentsMargins(0, 0, 0, 0);
+    rootLayout->setSpacing(0);
 
     // Боковая панель
     sidebar = new Sidebar(central);
     rootLayout->addWidget(sidebar, 1);
 
-    // Рабочая область
-    workspace = new Workspace(central);
-    rootLayout->addWidget(workspace, 3);
+    // Создаем QTabWidget для вкладок
+    tabWidget = new QTabWidget(central);
+
+    // Рабочая область (главная вкладка)
+    workspace = new Workspace(tabWidget);
+    tabWidget->addTab(workspace, "Книги");
+
+    // Настройка вкладок (можно закрывать)
+    tabWidget->setTabsClosable(true);
+    tabWidget->setMovable(true);
+
+    // Подключаем сигнал закрытия вкладки
+    connect(tabWidget, &QTabWidget::tabCloseRequested, this, [this](int index) {
+        QWidget *widget = tabWidget->widget(index);
+        if (widget != workspace) {  // Не закрываем главную вкладку
+            tabWidget->removeTab(index);
+            delete widget;
+        }
+    });
+
+    rootLayout->addWidget(tabWidget, 3);
 
     setCentralWidget(central);
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTabWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -24,6 +25,7 @@ private:
     Workspace *workspace;
     Sidebar *sidebar;
     Database *database;
+    QTabWidget *tabWidget;  // Добавляем QTabWidget
 };
 
 #endif
